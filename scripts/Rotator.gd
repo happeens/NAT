7df47d24efba27aa830dfae74536040bb9aab8c2
Hannes <hannes@guedelhoefer.de>
2017-11-18 13:33:35 +0100

    :bug: Fix natee not aiming corrrectly

--- scripts/Rotator.gd
+++ scripts/Rotator.gd
@@ -11,22 +11,22 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	look_at( get_viewport().get_mouse_pos() )
-	rotate(-PI/2 - 0.5)
+	look_at( get_viewport().get_mouse_position() )
+	rotate(+0.5)
 	
 	if (ammo < max_ammo && OS.get_unix_time() > last_recharge + recharge_duration):
 		ammo += 1
 		update_ammo_label()
 
-func _input(event):
-	if (
-		event.type == InputEvent.MOUSE_BUTTON &&
-		event.button_index == BUTTON_LEFT &&
-		event.pressed &&
-		ammo > 0
-	):
-		print("pew")
-		shoot(event.pos - get_pos())
+#func _input(event):	
+	#if (
+	#	event.type == InputEvent.MOUSE_BUTTON &&
+	#	event.button_index == BUTTON_LEFT &&
+	#	event.pressed &&
+	#	ammo > 0
+	#):
+	#	print("pew")
+	#	shoot(event.pos - get_pos())
 
 func shoot(where):
 	ammo -= 1
