3059780ecde3d4f39653e35978a3f46a7c0f916b
Leon Strauss <leon@ilu.st>
2017-11-18 17:32:18 +0100

    :sparkles: Add weapons base class

---
+++ scripts/Projectile.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+export var direction = Vector2(1, 0)
+var speed = 1000
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	translate(delta * speed * direction)
--- scripts/Weapon.gd
+++ scripts/Weapon.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 onready var ammo_label = get_node("Label")
 
+export(PackedScene) var shot
+
 var max_ammo = 5
 var ammo = max_ammo
 var last_recharge = OS.get_unix_time()
-var recharge_duration = .8
+
+const recharge_duration = .8
 
 func _ready():
 	set_process(true)
@@ -13,8 +16,7 @@ func _ready():
 
 func _process(delta):
 	look_at( get_viewport().get_mouse_position() )
-	rotate(+0.5)
-		
+	rotate(.5) #AAAAAAAAH
 	
 	var current_time = OS.get_unix_time()
 	
@@ -23,20 +25,21 @@ func _process(delta):
 		update_ammo_label()
 		last_recharge = current_time
 
-#func _input(event):	
-	#if (
-	#	event.type == InputEvent.MOUSE_BUTTON &&
-	#	event.button_index == BUTTON_LEFT &&
-	#	event.pressed &&
-	#	ammo > 0
-	#):
-	#	print("pew")
-	#	shoot(event.pos - get_pos())
+func _input(event):
+	if (event.is_action("shoot") && ammo > 0):
+		shoot(event.get_position() - get_position())
 
 func shoot(where):
+	var theShot = shot.instance()
+	print(theShot.direction)
+	
+	get_tree().get_root().add_child(theShot)
+	theShot.direction = where.normalized()
+	theShot.set_position(get_global_position())
+	#add_child(theShot)
+	
 	ammo -= 1
 	update_ammo_label()
 
 func update_ammo_label():
 	ammo_label.set_text( String(ammo) )
-	ammo_label.update()
