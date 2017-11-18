b40a2c8817cf6d64f3c1e6bb44e4f2172258f8e2
Hannes <hannes@guedelhoefer.de>
2017-11-18 21:27:08 +0100

    :sparkles: Add smoke particles to rocket

--- scenes/weapons/rocket/rocket-projectile.gd
+++ scenes/weapons/rocket/rocket-projectile.gd
@@ -1,4 +1,18 @@
 extends "res://scripts/Projectile.gd"
 
+export var SMOKE_TIME = 100;
+export(PackedScene) var smoke;
+
 func _ready():
 	speed = 750
+	
+var lastSmoke = 0
+func _process(delta):
+	lastSmoke += delta
+	if (lastSmoke > SMOKE_TIME):
+			lastSmoke -= SMOKE_TIME
+			var smokeParticle = smoke.instance()
+	
+			get_tree().get_root().add_child(smokeParticle)
+			smokeParticle.set_position(get_global_position())
+	
