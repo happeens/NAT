c6fac1534a5c4c6478584f1fe7e391ff60980535
Leon Strauss <leon@ilu.st>
2017-11-19 18:34:29 +0100

    :sparkles: Add rocket explosion

--- scenes/weapons/rocket/rocket-projectile.gd
+++ scenes/weapons/rocket/rocket-projectile.gd
@@ -1,12 +1,14 @@
 extends "res://scripts/Projectile.gd"
 
-export var SMOKE_TIME = 0.3;
-export(PackedScene) var smoke;
+export var SMOKE_TIME = 0.3
+export(PackedScene) var smoke
+export(PackedScene) var explosion
+
+var lastSmoke = 0
 
 func _ready():
 	speed = 750
-	
-var lastSmoke = 0
+
 func _process(delta):
 	lastSmoke += delta * (randf() * 7 - 0.5)
 	if (lastSmoke > SMOKE_TIME):
@@ -15,4 +17,13 @@ func _process(delta):
 	
 			get_tree().get_root().add_child(smokeParticle)
 			smokeParticle.set_position(get_global_position())
+
+func on_collide(partner):
+	if partner.get_name().begins_with("Player"):
+		partner.get_hit(1)
 	
+	var new_explosion = explosion.instance()
+	new_explosion.set_position(get_global_position())
+	get_node("/root").add_child(new_explosion)
+
+	queue_free()
