14a90643f98fcaab1cbae4d6892575af8c3c262e
Leon Strauss <leon@ilu.st>
2017-11-19 17:31:26 +0100

    :sparkles: Teach bullets to hurt

--- scenes/weapons/pistol/pistol-projectile.gd
+++ scenes/weapons/pistol/pistol-projectile.gd
@@ -2,3 +2,9 @@ extends "res://scripts/Projectile.gd"
 
 func _ready():
 	speed = 1300
+	
+func on_collide(partner):
+	if partner.get_name().begins_with("Player"):
+		partner.get_hit(1)
+		
+	queue_free()
--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -6,6 +6,13 @@ var speed = 1000
 func _ready():
 	set_process(true)
 	
+func _process(delta):
+	for body in get_colliding_bodies():
+		on_collide(body)
+	
 func set_direction(new_direction):
 	direction = new_direction
 	set_linear_velocity(direction * speed)
+
+func on_collide(partner):
+	pass
